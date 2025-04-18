a8d51db32716d58ee9b530570948369fe26c3c2f
KyleAngeles <kyle.angeles@dcmail.ca>
2025-04-17 20:38:13 -0400

    writing some math calculations

--- TiltingMaze/script.js
+++ TiltingMaze/script.js
@@ -25,3 +25,41 @@ const closestItCanBe = (cap, ball) => {
 
     return { x: cap.x + deltaX, y: cap.y + deltaY};
 }
+
+//Function to roll the ball around the wall cap
+const rollAroundWallCap = (cap, wall) => {
+    let impactAngle = getAngle(ball, cap);
+
+    //Direction the ball wants to move based on it's veloctuy
+    let heading = getAngle(
+        {x: 0, y: 0},
+        {x: ball.velocityX, y: ball.velocityY}
+    );
+
+    //Then angle between the impact direction and the ball's desired direction
+    // The smaller angle is, the bigger impact of the ball
+    // THe closer it is to 90 degrees the smoother it gets ( at 90 degrees ther would be no collision at all)
+    let impactHeadAngle = impactAngle - heading;
+
+    //Velocity distance if not hit would have occured 
+    const velocityMagnitude = distance2D (
+        {x: 0, y: 0},
+        {x: ball.velocityX, y: ball.velocityY} 
+    );
+
+    //Veloctiy component diagnoal to the impact
+    const velocityMagnitudeDiagonalToTheImpact =
+    Math.sin(impactHeadAngle) * velocityMagnitude;
+
+    //This calculates on how far should the ball be from the wall cap 
+    const closestDistance = wallW / 2 + ballSize / 2;
+
+    const rotationAngle = Math.atan(
+        velocityMagnitudeDiagonalToTheImpact / closestDistance
+    );
+
+    const deltaFromCap = {
+        x: Math.cos(impactAngle + Math.PI - rotationAngle) * closestDistance,
+        y: Math.sin(impactAngle + Math.PI - rotationAngle) * closestDistance
+    };
+}
