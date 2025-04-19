58ea73cd2354995012077ede0193536e7e05e695
KyleAngeles <kyle.angeles@dcmail.ca>
2025-04-18 23:15:32 -0400

    update
    change the score from 5 to 10, and now just adding the consts for the user interfaces

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -34,5 +34,24 @@ function getRandomChoice() {
             return 'ROCK'
         case 1:
             return 'SCISSORS';
+        case 2:
+            return 'PAPER';
     } 
 }
+
+function isGameOver() {
+    return playerScore === 10 || computerScore == 10
+}
+
+//UI / User interface 
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const PlayerscorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const scissorBtn = document.getElementById('scissorBtn')
+const paperBtn = document.getElementById('paperBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('')
